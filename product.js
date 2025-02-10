7d88168937ef31b86a7d2ac401ee5e28603751fd
Dhanushi10 <dhanushipanga10@gmail.com>
2025-02-09 22:15:24 -0500

    Add files via upload

---
+++ product.js
@@ -0,0 +1,39 @@
+import { redis } from "../lib/redis.js";
+import cloudinary from "../lib/cloudinary.js";
+import Product from "../models/product.model.js";
+
+export const getAllProducts = async (req, res) => {
+	try {
+		const products = await Product.find({}); // find all products
+		res.json({ products });
+	} catch (error) {
+		console.log("Error in getAllProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
+export const getFeaturedProducts = async (req, res) => {
+	try {
+		let featuredProducts = await redis.get("featured_products");
+		if (featuredProducts) {
+			return res.json(JSON.parse(featuredProducts));
+		}
+
+		// If not in Redis, fetch from MongoDB
+		featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+		if (!featuredProducts || featuredProducts.length === 0) {
+			return res.status(404).json({ message: "No featured products found" });
+		}
+
+		// Optionally cache the result in Redis for future requests
+		await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+		// Send the response
+		return res.json({ featuredProducts });
+
+	} catch (error) {
+		console.log("Error in getFeaturedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
